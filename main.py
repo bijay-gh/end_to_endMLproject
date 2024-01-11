d8ae818aa73442e1788005c1d4692d58699bb473
Bijaya Ghimire <vijay.gh74@gmail.com>
2024-01-11 10:57:03 +0545

    Completed Data injectiom

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 from mlproject import logger
+from mlproject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
+STAGE_NAME = "Data Injection Stage"
 
-logger.info("Logging from main.py")
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
